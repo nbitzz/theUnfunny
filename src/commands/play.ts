7a6c20fa47c52687db560b7d60e1e98541c05deb
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-11-22 20:42:10 -0800

    arrbu f

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -223,10 +223,7 @@ command.action = async (interaction) => {
     // get audio
 
     axios.get(file_url,{
-        responseType:"arraybuffer",
-        headers:{
-            "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-        }
+        responseType:"arraybuffer"
     }).then(async (dt) => {
         // make sure audio is audio/ or video/
         if (
