a83af288dd6bc57f9bcfa215c2953e716194345e
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-11-24 15:58:05 -0800

    server emojis

---
+++ assets/commands/smashorpass/generators/serverEmoji.js
@@ -0,0 +1,21 @@
+let {ChatInputCommandInteraction} = require("discord.js")
+
+/**
+ * 
+ * @param {ChatInputCommandInteraction} interaction 
+ * @returns 
+ */
+
+module.exports = function(interaction) {
+    return new Promise((resolve,reject) => {
+        if (interaction.guild) {
+            interaction.guild.emojis.fetch().then((emojis) => {
+                resolve(JSON.stringify(emojis.map((v) => {
+                    return {name:v.name,image:v.url}
+                })))
+            }).catch(e => reject("failed to get emojis"))
+        } else {
+            reject('guild not found');
+        }
+    })
+}
