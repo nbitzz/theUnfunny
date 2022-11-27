a72846b66d962c8e84924e61abe5697aa1c2264d
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-11-26 19:29:02 -0800

    w

--- assets/commands/smashorpass/generators/mori.js
+++ assets/commands/smashorpass/generators/mori.js
@@ -11,7 +11,7 @@ module.exports = function(interaction) {
     return new Promise(async (resolve,reject) => {
             axios.get("https://unusann.us/api/v1/gets01metadata").then(async (data) => {
                 resolve(JSON.stringify(data.data.map((v) => {
-                    return {name:v.title,image:"https://unusann.us"+((v.posters?(v.posters.find(e => e.type == "image/jpeg") || v.posters[0]).src:v.thumbnail)||"/thumbnails/00/001.jpg")}
+                    return {name:v.title,image:"https://cdn.unusannusarchive.tk/"+((v.posters?(v.posters.find(e => e.type == "image/jpeg") || v.posters[0]).src:v.thumbnail)||"/thumbnails/00/001.jpg")}
                 })))
             }).catch(reject)
     })
