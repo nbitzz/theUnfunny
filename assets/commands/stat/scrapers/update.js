714d13478875faa915abeecb5cc22ac85b511346
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-12-19 23:23:12 -0800

    key cell

--- assets/commands/stat/scrapers/update.js
+++ assets/commands/stat/scrapers/update.js
@@ -11,8 +11,8 @@ module.exports = function(interaction,data) {
     return new Promise((resolve,reject) => {
         resolve(data.slice(0,3).map((e) => {
             return {
-                name: `commit ${e.sha.slice(0,6)}`,
-                value: `authored by [${e.author.login}](${e.author.url})\n${e.commit.author.date}\n\`\`${e.commit.message.split("\n")[0]}\`\``
+                name: `commit ${e.sha.slice(0,7)}`,
+                value: `authored by [${e.author.login}](${e.author.html_url})\n${e.commit.author.date}\n\`\`${e.commit.message.split("\n")[0]}\`\`\n[view on GitHub](${e.html_url})`
             }
         }))
     })
