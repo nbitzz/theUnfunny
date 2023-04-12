ac5d2c813a75d7262071670a874dda1e4beb21da
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-04-12 11:33:20 -0700

    fix.... again....

--- src/lib/managers.ts
+++ src/lib/managers.ts
@@ -45,7 +45,7 @@ export class ChannelManagerController {
 
                 let manager:BaseChannelManager = 
                     require(process.cwd()+"/out/managers/"+name)
-                    .get( control, command )
+                    ( control, command )
                 
 
                 csle.success(`Loaded ${fn} as ${manager.name}`)
