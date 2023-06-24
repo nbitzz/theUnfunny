c8016cbb8846f56bb1d67d9b5ef63eb5a808aae6
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-06-23 20:56:29 -0700

    wrong way

--- assets/commands/smashorpass/generators/mori.js
+++ assets/commands/smashorpass/generators/mori.js
@@ -13,7 +13,7 @@ module.exports = function(interaction) {
                 resolve(JSON.stringify(data.data.map((v,x) => {
                     //https://unusann.us/_next/image?url=https%3A%2F%2Fstream.unusann.us%2Fthumbnails%2F01%2F001.webp&w=3840&q=100
                     //return {name:v.title,image:"https://cdn.unusannusarchive.tk/"+((v.posters?(v.posters.find(e => e.type == "image/jpeg") || v.posters[0]).src:v.thumbnail)||"/thumbnails/00/001.jpg")}
-                    return {name:v.title, image: `https://unusann.us/_next/image?url=https%3A%2F%2Fstream.unusann.us%2Fthumbnails%2F01%2F${`000`.slice(3-(x+1).toString().length)}${(x+1)}.webp&w=1920&q=100` }
+                    return {name:v.title, image: `https://unusann.us/_next/image?url=https%3A%2F%2Fstream.unusann.us%2Fthumbnails%2F01%2F${`000`.slice(-(3-(x+1).toString().length))}${(x+1)}.webp&w=1920&q=100` }
                 })))
             }).catch(reject)
     })
